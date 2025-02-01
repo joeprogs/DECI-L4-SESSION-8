d096bce2704b33a1380b4efd001d781840b04bfe
joeprogs <joeprogs@gmail.com>
2025-02-01 19:33:13 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DECI L4 Session 8 Activity
+
+### Team: Youssef Emad, Marina Magdy, Ziad Faseeh, Ahmed Moataz
---
+++ user_data.js
@@ -0,0 +1,13 @@
+fetch("https://randomuser.me/api").then((data) => {
+    data.json().then((json) => {
+        const email = json["results"][0]["email"];
+        const nameObject = json["results"][0]["name"];
+        const fullNameWithTitle = `${nameObject["title"]}. ${nameObject["first"]} ${nameObject["last"]}`;
+        console.log(`Name: ${fullNameWithTitle}`);
+        console.log(`Email: ${email}`);
+    }).catch(() => {
+        console.log("Could not get json data.");
+    });
+}).catch(() => {
+    console.log("Could not get a response.");
+});
