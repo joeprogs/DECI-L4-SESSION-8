82449763921e43e3121308ecbb1962f6853bfb23
joeprogs <joeprogs@gmail.com>
2025-02-01 19:39:35 +0200

    Formatted user_data.js

--- user_data.js
+++ user_data.js
@@ -1,13 +1,18 @@
-fetch("https://randomuser.me/api").then((data) => {
-    data.json().then((json) => {
+fetch("https://randomuser.me/api")
+  .then((data) => {
+    data
+      .json()
+      .then((json) => {
         const email = json["results"][0]["email"];
         const nameObject = json["results"][0]["name"];
         const fullNameWithTitle = `${nameObject["title"]}. ${nameObject["first"]} ${nameObject["last"]}`;
         console.log(`Name: ${fullNameWithTitle}`);
         console.log(`Email: ${email}`);
-    }).catch(() => {
+      })
+      .catch(() => {
         console.log("Could not get json data.");
-    });
-}).catch(() => {
+      });
+  })
+  .catch(() => {
     console.log("Could not get a response.");
-});
+  });
